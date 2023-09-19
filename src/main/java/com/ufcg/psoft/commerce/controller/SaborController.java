f380fd40378fbe985f206220a1325d9d3f635cd3
Iury Barbosa <iury.alves@ccc.ufcg.edu.com>
2023-09-18 22:11:31 -0300

    feat: Us1 completed

--- src/main/java/com/ufcg/psoft/commerce/controller/SaborController.java
+++ src/main/java/com/ufcg/psoft/commerce/controller/SaborController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/v1/sabores", produces = MediaType.APPLICATION_JSON_VALUE)
-public class SaborV1RestController {
+public class SaborController {
 
         @Autowired
         private ModelMapper modelMapper;
